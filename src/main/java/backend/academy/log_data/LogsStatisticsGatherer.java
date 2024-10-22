11a069d5e29b1b2f54b66a56af9f461c2d42bd76
nextupvamp <k.vlad.200404@gmail.com>
2024-10-22 18:25:14 +0300

    init commit

---
+++ src/main/java/backend/academy/log_data/LogsStatisticsGatherer.java
@@ -0,0 +1,130 @@
+package backend.academy.log_data;
+
+import backend.academy.handlers.LogPaths;
+import backend.academy.handlers.log_handlers.LogsHandler;
+import backend.academy.io.LogsReader;
+import java.net.URL;
+import java.nio.file.Path;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+import lombok.SneakyThrows;
+
+public class LogsStatisticsGatherer {
+    private final List<String> readPaths = new ArrayList<>();
+    private final List<Long> bytesSent = new ArrayList<>();
+    private final Map<String, Integer> remoteAddresses = new HashMap<>();
+    private final Map<String, Integer> remoteUsers = new HashMap<>();
+    private final Map<String, Integer> requestMethods = new HashMap<>();
+    private final Map<String, Integer> requestResources = new HashMap<>();
+    private final Map<Short, Integer> statuses = new HashMap<>();
+    private final AtomicInteger requestsAmount = new AtomicInteger(0);
+    private final ZonedDateTime from;
+    private final ZonedDateTime to;
+    private final LogPaths paths;
+    private final LogsHandler logsHandler;
+    private final String filterMethod;
+    private final Pattern filterValueRegex;
+    private final Map<ZonedDateTime, Integer> requestsOnDate = new HashMap<>();
+    private Predicate<LogData> dateTimePredicate;
+    private Predicate<LogData> fieldFilterPredicate;
+
+    public LogsStatisticsGatherer(
+        LogPaths paths,
+        final ZonedDateTime from,
+        final ZonedDateTime to,
+        String filterMethod,
+        Pattern filterValueRegex,
+        LogsHandler logsHandler
+    ) {
+        this.from = from;
+        this.to = to;
+        this.filterMethod = filterMethod;
+        this.filterValueRegex = filterValueRegex;
+        this.paths = paths;
+        this.logsHandler = logsHandler;
+    }
+
+    @SneakyThrows
+    public LogsStatistics gatherStatistics() {
+        try (LogsReader logsReader = new LogsReader()) {
+            List<Path> files = paths.paths();
+            List<URL> urls = paths.urls();
+
+            if (files != null) {
+                for (Path path : files) {
+                    Path fileName = path.getFileName();
+                    if (fileName != null) {
+                        readPaths.add(fileName.toString());
+                    }
+
+                    gatherData(logsReader.readFromFileAsStream(path, logsHandler));
+                }
+            }
+
+            if (urls != null) {
+                for (URL url : urls) {
+                    readPaths.add(url.getFile());
+                    gatherData(logsReader.readFromUrlAsStream(url, logsHandler));
+                }
+            }
+        }
+
+        return new LogsStatistics(
+            readPaths,
+            remoteAddresses,
+            remoteUsers,
+            from,
+            to,
+            requestsOnDate,
+            requestMethods,
+            requestResources,
+            statuses,
+            requestsAmount.get(),
+            countAverageBytesSent(bytesSent),
+            count95pBytesSent(bytesSent)
+        );
+    }
+
+    @SuppressWarnings({"checkstyle:IllegalIdentifierName", "checkstyle:LambdaParameterName"})
+    private void gatherData(Stream<LogData> logDataStream) {
+        logDataStream
+            .filter(dateTimePredicate)
+            .filter(fieldFilterPredicate)
+            .peek(_ -> requestsAmount.incrementAndGet())
+            .forEach(it -> {
+                bytesSent.add(it.bytesSent());
+                remoteAddresses.merge(it.remoteAddress(), 1, Integer::sum);
+                remoteUsers.merge(it.remoteUser(), 1, Integer::sum);
+                requestMethods.merge(it.requestMethod(), 1, Integer::sum);
+                requestResources.merge(it.requestResource(), 1, Integer::sum);
+                statuses.merge(it.status(), 1, Integer::sum);
+                requestsOnDate.merge(it.timeLocal(), 1, Integer::sum);
+            });
+    }
+
+    public static long countAverageBytesSent(List<Long> bytesSent) {
+        if (bytesSent.isEmpty()) {
+            return 0;
+        }
+
+        return bytesSent.stream().reduce(0L, Long::sum) / bytesSent.size();
+    }
+
+    @SuppressWarnings("checkstyle:MagicNumber")
+    public static long count95pBytesSent(List<Long> bytesSent) {
+        if (bytesSent.isEmpty()) {
+            return 0;
+        }
+        bytesSent.sort(Long::compareTo);
+
+        return bytesSent.get((int) (bytesSent.size() * 0.95));
+    }
+}
+
