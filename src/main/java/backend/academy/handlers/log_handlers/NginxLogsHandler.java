d814338a251ce07ed7a241acf7e3d346bbd4e476
nextupvamp <k.vlad.200404@gmail.com>
2024-10-22 18:20:51 +0300

    init commit

---
+++ src/main/java/backend/academy/handlers/log_handlers/NginxLogsHandler.java
@@ -0,0 +1,11 @@
+package backend.academy.handlers.log_handlers;
+
+import backend.academy.log_data.LogData;
+
+public class NginxLogsHandler implements LogsHandler {
+
+    @Override
+    public LogData parseLogLineData(String line) {
+        return null;
+    }
+}
