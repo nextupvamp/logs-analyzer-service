3ee06e320757a0cbc12421604a0bc148b1fc242e
nextupvamp <k.vlad.200404@gmail.com>
2024-10-23 15:38:14 +0300

    renamed class, moved main application logics there

--- src/main/java/backend/academy/Application.java
+++ src/main/java/backend/academy/Application.java
@@ -2,18 +2,23 @@ package backend.academy;
 
 import backend.academy.data.ArgsData;
 import backend.academy.handlers.ArgsHandler;
+import backend.academy.handlers.log_handlers.LogsStatisticsGatherer;
 import backend.academy.handlers.log_handlers.NginxLogsHandler;
 import backend.academy.io.ReportCreator;
-import backend.academy.handlers.log_handlers.LogsStatisticsGatherer;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Instant;
 import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
-public class Main {
+public class Application {
     @SneakyThrows
+    @SuppressFBWarnings // suppress warnings concerned with file/url user input
     public static void main(String[] args) {
         ArgsHandler argsHandler = new ArgsHandler(args);
         ArgsData argsData = argsHandler.handle();
@@ -27,8 +32,10 @@ public class Main {
             new NginxLogsHandler()
         );
 
-        Path file = Path.of("C:\\Users\\Владислав\\log_report.md");
-        PrintStream printStream = new PrintStream(file.toFile(), StandardCharsets.UTF_8);
+        String fileFormat = argsData.format().getFileFormat();
+        Path reportFile = Paths.get("report" + Instant.now().toEpochMilli() + fileFormat);
+
+        PrintStream printStream = new PrintStream(Files.newOutputStream(reportFile), true, StandardCharsets.UTF_8);
         ReportCreator.createReport(statisticsGatherer.gatherStatistics(), argsData.format(), printStream);
     }
 }
