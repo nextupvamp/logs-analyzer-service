f479a478a6f8286499fa111082bae43db0a4c8e4
nextupvamp <k.vlad.200404@gmail.com>
2024-10-24 23:15:48 +0300

    corrected class name, URL changed to URI

--- src/main/java/backend/academy/data/PathsData.java
+++ src/main/java/backend/academy/data/PathsData.java
@@ -1,11 +1,11 @@
 package backend.academy.data;
 
-import java.net.URL;
+import java.net.URI;
 import java.nio.file.Path;
 import java.util.List;
 
-public record LogPaths(
-    List<URL> urls,
+public record PathsData(
+    List<URI> uris,
     List<Path> paths
 ) {
 }
