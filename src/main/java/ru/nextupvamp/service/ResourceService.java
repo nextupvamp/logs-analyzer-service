901300f107c833150caa2e993a8758a43f4e29ea
nextupvamp <k.vlad.200404@gmail.com>
2025-01-19 11:45:29 +0300

    added changes to support new user logics

--- src/main/java/ru/nextupvamp/service/ResourceService.java
+++ src/main/java/ru/nextupvamp/service/ResourceService.java
@@ -6,13 +6,10 @@ import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import ru.nextupvamp.model.data.Filters;
-import ru.nextupvamp.model.data.LogsStatistics;
-import ru.nextupvamp.model.entities.Resource;
-import ru.nextupvamp.model.entities.ResourceFilters;
-import ru.nextupvamp.model.entities.ResourceType;
+import ru.nextupvamp.model.entities.*;
 import ru.nextupvamp.model.handlers.LogsStatisticsGatherer;
 import ru.nextupvamp.repository.ResourceRepository;
+import ru.nextupvamp.repository.UserRepository;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -28,10 +25,31 @@ public class LogsAnalyzerService {
     @Value("${file.user-file-directory}")
     private String userFilesDirectory;
     private final ResourceRepository resourceRepository;
+    private final UserRepository userRepository;
     private final LogsStatisticsGatherer logsStatisticsGatherer;
 
     public LogsStatistics getStatistics(int id) {
         var resource = resourceRepository.findById(id).orElseThrow();
+    public Statistics getSavedStatistics(int resourceId) {
+        return resourceRepository.findById(resourceId).orElseThrow(NO_RESOURCE_WITH_SUCH_ID).statistics();
+    }
+
+    public Statistics getUpdatedStatistics(int resourceId) {
+        var resource = resourceRepository.findById(resourceId).orElseThrow(NO_RESOURCE_WITH_SUCH_ID);
+        var statistics = gatherStatistics(resource);
+        resource.statistics(statistics);
+        resourceRepository.save(resource);
+        return statistics;
+    }
+
+    public Statistics getStatisticsOnce(int resourceId) {
+        var resource = resourceRepository.findById(resourceId).orElseThrow(NO_RESOURCE_WITH_SUCH_ID);
+        var statistics = gatherStatistics(resource);
+        resourceRepository.delete(resource);
+        return statistics;
+    }
+
+    private Statistics gatherStatistics(Resource resource) {
         return switch (resource.type()) {
             case FILE -> getStatisticsFromFile(resource);
             case URL -> getStatisticsFromUri(resource);
@@ -47,37 +65,26 @@ public class LogsAnalyzerService {
         return logsStatisticsGatherer.gatherStatisticsFromFile(file, filters);
     }
 
-    private LogsStatistics getStatisticsFromUri(Resource resource) {
+    private Statistics getStatisticsFromUri(Resource resource) {
         var uri = URI.create(resource.path());
-
         var resourceFilters = resource.filters();
-        Filters filters = mapResourceFiltersToFilters(resourceFilters);
-
-        return logsStatisticsGatherer.gatherStatisticsFromUri(uri, filters);
-    }
-
-    private Filters mapResourceFiltersToFilters(ResourceFilters resourceFilters) {
-        if (resourceFilters == null) {
-            return Filters.EMPTY;
-        }
-
-        return Filters.builder()
-                .fromDate(resourceFilters.fromDate())
-                .toDate(resourceFilters.toDate())
-                .filterMap(resourceFilters.filterMap())
-                .build();
+        return logsStatisticsGatherer.gatherStatisticsFromUri(uri, resourceFilters);
     }
 
     @SneakyThrows
-    public int uploadFile(MultipartFile file) {
+    public int uploadFile(MultipartFile file, User user) {
+        User persistentUser = null;
+        if (user != null && user.login() != null && !user.login().isEmpty()) {
+            persistentUser = userRepository.findById(user.login()).orElseThrow(USER_NOT_FOUND);
+        }
+
         String path = getFreeFileName();
         File actualFile = new File(path);
         @Cleanup FileOutputStream fos = new FileOutputStream(actualFile);
         fos.write(file.getBytes());
 
         var resource = new Resource();
-        resource.type(ResourceType.FILE);
-        resource.path(path);
+        resource.type(ResourceType.FILE).path(path).user(persistentUser);
         resourceRepository.save(resource);
         return resource.id();
     }
@@ -92,7 +99,12 @@ public class LogsAnalyzerService {
     }
 
     @SuppressWarnings("ResultOfMethodCallIgnored")
-    public int uploadUrl(String url) {
+    public int uploadUrl(String url, User user) {
+        User persistentUser = null;
+        if (user != null && user.login() != null && !user.login().isEmpty()) {
+            persistentUser = userRepository.findById(user.login()).orElseThrow(USER_NOT_FOUND);
+        }
+
         try {
             URI.create(url);
         } catch (NullPointerException e) {
@@ -102,21 +114,23 @@ public class LogsAnalyzerService {
         }
 
         var resource = new Resource();
-        resource.type(ResourceType.URL);
-        resource.path(url);
+        resource.type(ResourceType.URL).path(url).user(persistentUser);
         resourceRepository.save(resource);
         return resource.id();
     }
 
-    public int uploadFilters(int id, Filters filters) {
+    public int uploadFilters(int id, ResourceFilters filters) {
         validateFilters(filters);
 
-        var resource = resourceRepository.findById(id).orElseThrow();
+        var resource = resourceRepository.findById(id).orElseThrow(NO_RESOURCE_WITH_SUCH_ID);
 
         var resourceFilters = new ResourceFilters();
         resourceFilters.fromDate(filters.fromDate());
         resourceFilters.toDate(filters.toDate());
         resourceFilters.filterMap(filters.filterMap());
+        resourceFilters.fromDate(filters.fromDate())
+                .toDate(filters.toDate())
+                .filterMap(filters.filterMap());
 
         resource.filters(resourceFilters);
         resourceRepository.save(resource);
@@ -124,7 +138,7 @@ public class LogsAnalyzerService {
         return id;
     }
 
-    private void validateFilters(Filters filters) {
+    private void validateFilters(ResourceFilters filters) {
         if (filters == null) {
             throw new IllegalArgumentException("Filters is null");
         }
@@ -136,19 +150,21 @@ public class LogsAnalyzerService {
         if (from != null && to != null && from.isAfter(to)) {
             throw new IllegalArgumentException("From time must not be after to time");
         }
-        filterMap.forEach((field, value) -> {
-            if (field == null && value != null) {
-                throw new IllegalArgumentException("Missing filter field for value " + value);
-            }
-            if (field != null && value == null) {
-                throw new IllegalArgumentException("Missing filter value regex for field " + field);
-            }
-        });
+        if (filterMap != null) {
+            filterMap.forEach((field, value) -> {
+                if (field == null && value != null) {
+                    throw new IllegalArgumentException("Missing filter field for value " + value);
+                }
+                if (field != null && value == null) {
+                    throw new IllegalArgumentException("Missing filter value regex for field " + field);
+                }
+            });
+        }
     }
 
     @SneakyThrows
     public void deleteResource(int id) {
-        var resource = resourceRepository.findById(id).orElseThrow();
+        var resource = resourceRepository.findById(id).orElseThrow(NO_RESOURCE_WITH_SUCH_ID);
         if (resource.type() == ResourceType.FILE) {
             Files.delete(Path.of(resource.path()));
         }
