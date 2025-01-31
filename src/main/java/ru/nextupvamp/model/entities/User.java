32bae289f2d51de6ce6aef831439f5eee0c21683
nextupvamp <k.vlad.200404@gmail.com>
2025-01-31 22:26:18 +0300

    removed unnecessary validation

--- src/main/java/ru/nextupvamp/model/entities/User.java
+++ src/main/java/ru/nextupvamp/model/entities/User.java
@@ -3,8 +3,6 @@ package ru.nextupvamp.model.entities;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,14 +16,8 @@ import java.util.List;
 @Table(name = "user_table")
 public class User {
     @Id
-    @NotBlank(message = "Login is mandatory")
-    @Size(min = 6, message = "Login is too short")
-    @Size(max = 20, message = "Login is too long")
     private String login;
 
-    @NotBlank(message = "Password is mandatory")
-    @Size(min = 8, message = "Password is too short")
-    @Size(max = 20, message = "Password is too long")
     private String password;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
